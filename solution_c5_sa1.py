a0d9a1a676796732226c0b9c479800d33d882327
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-10-19 07:08:36 +0530

    Update solution_c5_sa1.py

--- solution_c5_sa1.py
+++ solution_c5_sa1.py
@@ -1,3 +1,4 @@
+# Importing 'random' 
 import pygame,math,random
 
 pygame.init()
@@ -6,11 +7,15 @@ screen = pygame.display.set_mode((400,600))
 pygame.display.set_caption("Shooting Spaceship")
 background_image = pygame.image.load("bg2.jpg").convert()
 player_image = pygame.image.load("s4.png").convert_alpha()
+
+# Loading the "e3.png" and name it as 'enemy_image'
 enemy_image = pygame.image.load("e3.png").convert_alpha()
+
 player=pygame.Rect(200,200,30,30)
 
-WHITE=(255,255,255)
-enemy=pygame.Rect(100,100,30,30)
+# The following lines are commented as it is not required anymore
+#WHITE=(255,255,255)
+#enemy=pygame.Rect(100,100,30,30)
 
 angle=0
 change=0
@@ -21,14 +26,18 @@ forward=False
 xvel=[]
 yvel=[]
 enemies=[]
-enemycount =10
+# Creating a variable 'enemycount' to store the number of enemies
+enemycount = 10
 
-# Creating 10 'enemy' rectangles at random x,y coordinates
-# Assigning 10 random values to 'xvel', 'yvel' in the range of -3 to 3
+# 'for' loop to repeat the task
 for i in range(enemycount): 
+  # Creating 10 'enemy' rectangles at random x,y coordinates
   enemies.append(pygame.Rect(random.randint(0,400),random.randint(0,600),20,20))
+  # Assigning 10 random values to 'xvel' in the range of -3 to 3
   xvel.append(random.randint(-3,3))
+  # Assigning 10 random values to 'yvel' in the range of -3 to 3
   yvel.append(random.randint(-3,3))
+  
 
 def newxy(x,y,distance,angle):
   angle=math.radians(angle+90)
@@ -56,21 +65,22 @@ while True:
       if event.key == pygame.K_UP:
         forward = False
         
-  # Initialise 'i' to 0. 'i' is used as 'index' variable to access every element of 'xvel' and 'yvel'
+  # Initialising 'i' to 0. 'i' is used as 'index' variable to access every element of 'xvel' and 'yvel'
   i=0    
-  # For every 'enemy' in the 'enemies' list do the following
-  # Increment x,y coordinates
-  # Checking if x-coordinate is less than -250 or greater than 650 and change direction
-  # Checking if y-coordinate is less than -250 or greater than 850 and change direction
-  # Display it on the screen and increment 'i'
+ 
+  # For every 'enemy' in the 'enemies' list do the following 
   for enemy in enemies:
+      # Incrementing x coordinate with 'xvel[i]'
       enemy.x=enemy.x + xvel[i]
-      enemy.y=enemy.y + yvel[i]      
+      # Checking if x-coordinate is less than -250 or greater than 650 and change direction
       if enemy.x < -250 or enemy.x > 650 :
         xvel[i] = -1*xvel[i]
-      if enemy.y < -250 or enemy.y > 850:  
+      # Checking if y-coordinate is less than -250 or greater than 850 and change direction
+      if enemy.y < -250 or enemy.y > 850 :
         yvel[i] = -1*yvel[i]
+      # Incrementing 'i' by '1'
       i+=1    
+      # Displaying 'enemy_image' on 'enemy' rectangle on the screen
       screen.blit(enemy_image,enemy)  
      
   if forward:
@@ -87,7 +97,8 @@ while True:
   newimage=pygame.transform.rotate(player_image,angle) 
   screen.blit(newimage ,player)
   
-  pygame.draw.rect(screen,WHITE,enemy)
+  # The following line is commented as it is not required anymore
+  #pygame.draw.rect(screen,WHITE,enemy)
 
   pygame.display.update()
   pygame.time.Clock().tick(30)
